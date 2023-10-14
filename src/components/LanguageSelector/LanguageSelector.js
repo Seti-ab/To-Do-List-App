1eb7c741d8c6ad5fdf0a0760578beca6d3e05ad4
Seti_ab <95739100+Seti-ab@users.noreply.github.com>
2023-10-14 22:45:34 +0330

    language Selector :  country flags and react-select added

--- src/components/LanguageSelector/LanguageSelector.js
+++ src/components/LanguageSelector/LanguageSelector.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import styles from "./LanguageSelector.module.scss";
+import Farsi from "../../assets/icons/farsi.png";
+import English from "../../assets/icons/english.png";
+import Select from 'react-select';
 
-const LanguageSelector = ({lcoale,setLocale}) => {
+const LanguageSelector = ({ locale, setLocale }) => {
 
-    const handleLanguageChange = (e) => {
-        setLocale(e.target.value);
+    const handleLanguageChange = (selected) => {
+        setLocale(selected);
     }
+    const options = [
+        { value: 'en', label: <div><img src={English} height="30px" width="30px" />English </div> },
+        { value: 'fa', label: <div><img src={Farsi} height="30px" width="30px" />Farsi </div> },
+    ];
+    console.log("locale", locale)
 
     return (
         <div styles={styles.container}>
             <form>
-                <select value={lcoale} onChange={handleLanguageChange}>
-                    <option value="en">English</option>
-                    <option value="fa">Farsi</option>
-                </select>
+                <Select
+                    onChange={handleLanguageChange}
+                    options={options}
+                    isSearchable={false}
+                    defaultValue={options[0]}
+                />
             </form>
-        </div>
+        </div >
     )
 }
 
