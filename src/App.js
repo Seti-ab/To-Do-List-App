49772d2fc389ada9c0caa6c5f9d32656e7303e1e
Setayesh Abouei <setayesh.ab1998@gmail.com>
2023-10-26 14:58:14 +0330

    initial theme component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import ToDoList from './components/ToDoList.js/ToDoList';
-import "./App.scss"
-import LanguageSelector from './components/LanguageSelector/LanguageSelector';
-import { useState } from 'react';
+import ToDoList from "./components/ToDoList.js/ToDoList";
+import "./App.scss";
+import LanguageSelector from "./components/LanguageSelector/LanguageSelector";
+import { useState } from "react";
 
 function App() {
-
   const [locale, setLocale] = useState(localStorage.getItem("locale"));
-
+  const [theme, setTheme] = useState();
+  
   return (
     <div className="App" data-theme="dark">
       <LanguageSelector locale={locale} setLocale={setLocale} />
