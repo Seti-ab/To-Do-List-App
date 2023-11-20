08750b70fb22999bb1b662a99040be9063d7a959
Seti_ab <95739100+Seti-ab@users.noreply.github.com>
2023-11-20 13:41:09 +0330

    another approach for PWA

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,4 +11,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
 reportWebVitals();
